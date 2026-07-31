3b3ba57d114e9a76b7d5b7c59af08c34bcbb6885
User <stivenalejandroyangy@gmail.com>
2026-07-31 09:38:52 -0500

    feat/Desarrollo de la evaluacion final con html, css y js

---
+++ script.js
@@ -0,0 +1,41 @@
+    const boton =  document.getElementById("boton1");
+    boton.addEventListener('click', function(){
+    let precio1 = 70;
+    const descu3nto = 0.15;
+    let precioF = precio1 * (1-descu3nto);
+    
+        alert("Frecio Final:" +precioF);
+    });
+
+
+    const boton2 =  document.getElementById("boton2");
+    boton.addEventListener('click', function(){
+    let precio2 = 50;
+    const descu3nto = 0.15;
+    let precioF = precio2 * (1-descu3nto);
+        alert("Frecio Final:" +precioF);
+    });
+
+    const boton3 =  document.getElementById("boton3");
+    boton.addEventListener('click', function(){
+    let precio3 = 5;
+    const descu3nto = 0.15;
+    let precioF = precio3 * (1-descu3nto);
+        alert("Frecio Final:" +precioF);
+    });
+
+    const boton4 =  document.getElementById("boton4");
+    boton.addEventListener('click', function(){
+    let precio4 = 30;
+    const descu3nto = 0.15;
+    let precioF = precio4 * (1-descu3nto);
+        alert("Frecio Final:" +precioF);
+    });
+
+    const boton5 =  document.getElementById("boton5");
+    boton.addEventListener('click', function(){
+    let precio5 = 25;
+    const descu3nto = 0.15;
+    let precioF = precio5 * (1-descu3nto);
+        alert("Frecio Final:" +precioF);
+    });
